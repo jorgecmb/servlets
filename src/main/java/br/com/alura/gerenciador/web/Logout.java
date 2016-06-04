3d003dabd78eeef92bf994cdfdabdbab6660a1bc
Jorge Calmon Neto <jorgecmb@Mac-mini-de-Jorge-Neto.local>
2016-06-03 21:24:55 -0400

    Initializing the repository

---
+++ src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -0,0 +1,15 @@
+package br.com.alura.gerenciador.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Logout implements Tarefa {
+
+	public String executa(HttpServletRequest request,
+			HttpServletResponse response) {
+		request.getSession().removeAttribute("usuarioLogado");
+		return "WEB-INF/paginas/logout.html";
+	}
+	
+
+}
---
+++ src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -0,0 +1,21 @@
+package br.com.alura.gerenciador.web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+
+@WebServlet(urlPatterns = "/novaEmpresa")
+public class NovaEmpresa implements Tarefa {
+
+	@Override
+	public String executa(HttpServletRequest request,
+			HttpServletResponse response) {
+		Empresa empresa = new Empresa(request.getParameter("nome"));
+		new EmpresaDAO().adiciona(empresa);
+		request.setAttribute("empresa", empresa);
+		return "/WEB-INF/paginas/novaEmpresa.jsp";
+	}
+}
